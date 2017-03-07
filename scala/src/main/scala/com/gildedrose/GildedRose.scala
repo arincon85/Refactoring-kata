8f4489eacfdba448c25c40a9cc9afd4adfb965c7
arinconstrio <arincon@stratio.com>
2017-03-07 10:22:43 +0100

    refactor to pattern matching

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -2,57 +2,70 @@ package com.gildedrose
 
 class GildedRose(val items: Seq[Item]) {
 
-
   def updateQuality() {
-    for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
-          }
-        }
-      } else {
-        if (items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
-
-          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).sellIn < 11) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-
-            if (items(i).sellIn < 6) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-          }
-        }
-      }
-
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-        items(i).sellIn = items(i).sellIn - 1
-      }
-
-      if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
-              }
-            }
-          } else {
-            items(i).quality = items(i).quality - items(i).quality
-          }
-        } else {
-          if (items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
-          }
-        }
+    for (item <- items) {
+      item.name match {
+        case ("Aged Brie") => agedBrie(item)
+        case ("Backstage passes to a TAFKAL80ETC concert") => backStage(item)
+        case ("Sulfuras, Hand of Ragnaros") => sulfuras(item)
+        case (_) => other(item)
       }
     }
+  }
+
+  private def agedBrie(item: Item) = {
+    item.sellIn = item.sellIn - 1
+    if (qualityLessThanMax(item)) {
+      item.quality = item.quality + 1
+      if (sellInDatePassed(item)) {
+        item.quality = item.quality + 1
+      }
+    }
+  }
+
+
+  private def backStage(item: Item) = {
+    item.sellIn = item.sellIn - 1
+    if (qualityLessThanMax(item)) {
+      item.quality = item.quality + 1
+      if (item.sellIn < 11) {
+        if (qualityLessThanMax(item)) {
+          item.quality = item.quality + 1
+        }
+      }
+      if (item.sellIn < 6) {
+        if (qualityLessThanMax(item)) {
+          item.quality = item.quality + 1
+        }
+      }
+      if (sellInDatePassed(item)) {
+        item.quality = item.quality - item.quality
+      }
+    }
+  }
+
+
+  private def sulfuras(item: Item) = {}
+
+  private def other(item: Item) = {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1
+    }
+    item.sellIn = item.sellIn - 1
+    if (sellInDatePassed(item)) {
+      if (item.quality > 0) {
+        item.quality = item.quality - 1
+      }
+    }
+  }
+
+
+
+  private def qualityLessThanMax(item: Item) = {
+    item.quality < 50
+  }
+
+  private def sellInDatePassed(item: Item) = {
+    item.sellIn < 0
   }
 }
